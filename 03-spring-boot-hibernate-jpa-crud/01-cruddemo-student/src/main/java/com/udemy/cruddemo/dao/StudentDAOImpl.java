6290d5c9effc7c64318bb49d7361f629b3d4b970
Edward Y. Huang <eddiehuang@hotmail.com>
2023-04-16 19:03:15 +0800

    Section 3 ep 85 Deleting Object with JPA

--- 03-spring-boot-hibernate-jpa-crud/01-cruddemo-student/src/main/java/com/udemy/cruddemo/dao/StudentDAOImpl.java
+++ 03-spring-boot-hibernate-jpa-crud/01-cruddemo-student/src/main/java/com/udemy/cruddemo/dao/StudentDAOImpl.java
@@ -2,9 +2,12 @@ package com.udemy.cruddemo.dao;
 
 import com.udemy.cruddemo.entity.Student;
 import jakarta.persistence.EntityManager;
+import jakarta.persistence.TypedQuery;
 import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public class StudentDAOImpl implements StudentDAO {
@@ -25,5 +28,57 @@ public class StudentDAOImpl implements StudentDAO {
     @Transactional
     public void save(Student theStudent) {
         entityManager.persist(theStudent);
+    }
+
+    @Override
+    public Student findById(Integer id) {
+        return entityManager.find(Student.class, id);
+    }
+
+    @Override
+    public List<Student> findAll() {
+        // create query
+        TypedQuery<Student> theQuery = entityManager.createQuery("FROM Student ORDER BY lastName", Student.class);
+
+        //return query results
+        return theQuery.getResultList();
+    }
+
+    @Override
+    public List<Student> findByLastName(String theLastName) {
+        // create query
+        TypedQuery<Student> theQuery = entityManager.createQuery("FROM Student WHERE lastName=:theData", Student.class);
+
+        // set query parameter
+        theQuery.setParameter("theData", theLastName);
+
+        //return query results
+        return theQuery.getResultList();
+    }
+
+    @Override
+    @Transactional
+    public void update(Student theStudent) {
+        entityManager.merge(theStudent);
+    }
+
+    @Override
+    @Transactional
+    public void delete(Integer id) {
+
+        // retrieve the student
+        Student theStudent = entityManager.find(Student.class, id);
+
+        // delete the student
+        entityManager.remove(theStudent);
+    }
+
+    @Override
+    @Transactional
+    public int deleteAll() {
+
+        int numRowsDeleted = entityManager.createQuery("DELETE FROM Student").executeUpdate();
+
+        return numRowsDeleted;
     }
 }
